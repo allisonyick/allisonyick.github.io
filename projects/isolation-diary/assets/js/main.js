dad44259129251e36b97d1e8b6d7526f883c141b
allisonyick <allison.yick@newschool.edu>
2020-04-20 23:18:00 -0400

    remove play method from video click function in main.js

--- projects/isolation-diary/assets/js/main.js
+++ projects/isolation-diary/assets/js/main.js
@@ -37,14 +37,10 @@ $(".next").hover(function(){
   $(this).css("border-bottom", "7vw solid black");
 });
 
-// var backgroundChange = css
-
-$('.preview').on('click', function(){
+$('video').on('click', function(){
   console.log("enjoy the video!");
-  // $(this)[0].play();
-  $('video').trigger('play');
-  $('.left').css('background-color', 'black');
-  $('.left').css('transition', '1s ease-in-out');
+  $('.left').css({'background-color': 'black', 
+                  'transition': '1s ease-in-out'});
   $('.view').css('border', '0.2vw solid black');
   $('.selected').css('border', '0.2vw solid snow');
   $('a').css('color', 'snow');
